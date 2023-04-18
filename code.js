17c12b9ac243fd3426c51f14f316800779623209
dilanurbayraktar <122991745+dilanurbayraktar@users.noreply.github.com>
2023-04-18 13:04:07 -0400

    Update code.js

--- code.js
+++ code.js
@@ -3,7 +3,7 @@ var port = process.env.PORT || 3000;
 console.log("This goes to the console window");
 http.createServer(function (req, res) {
 res.writeHead(200, {'Content-Type': 'text/html'});
-res.write("Hello World");
+res.write("<h2>Hello World</h2>");
 res.write ("Success! This app is deployed online");
 res.end();
 }).listen(port);
